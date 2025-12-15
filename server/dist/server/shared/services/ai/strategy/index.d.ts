92f345d35260ea7655e8f13c636baf82b64f787a
Azer-of-Sunbreach <gissaly1991@gmail.com>
2025-12-15 20:18:29 +0100

    Delete server/dist/server/shared/services/ai directory

--- server/dist/server/shared/services/ai/strategy/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './types';
-export { analyzeTheaters } from './theaters';
-export { generateDefendMissions } from './defenseMissions';
-export { generateCampaignMissions } from './campaignMissions';
-export { generateDiplomacyMissions } from './diplomacyMissions';
-export { generateRoadDefenseMissions } from './roadDefense';
