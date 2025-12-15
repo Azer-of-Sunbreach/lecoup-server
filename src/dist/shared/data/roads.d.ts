a45d9b51888b4c5e43558c5521634981eca86643
Azer-of-Sunbreach <gissaly1991@gmail.com>
2025-12-15 19:54:38 +0100

    Add files via upload

---
+++ src/dist/shared/data/roads.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Road Network Data - All roads and road stages connecting territories
+ * Derived from Spec 2.3 (Road Network)
+ */
+import { Road } from '../types';
+export declare const ROADS: Road[];
